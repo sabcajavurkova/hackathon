fb8a465db6c18c50803a2305ffb9c5fab0d5c6d1
sabi <sabina.javurkova@student.gyarab.cz>
2024-11-29 00:27:57 +0100

    registrace+prihlaseni

--- application/forms.py
+++ application/forms.py
@@ -2,8 +2,22 @@ from django import forms
 from django.core.validators import MinLengthValidator
 from .models import Student, Teacher, Lecture
 
-class RegisterUserForm(forms.ModelForm):
+class RegisterUserForm(forms.Form):
+    GROUP_CHOICES = (
+        ('Ucitel', 'Ucitel'),
+        ('Zak', 'Zak')
+    )
+    role = forms.ChoiceField(
+        choices=GROUP_CHOICES,
+        widget=forms.Select,
+        required=True,
+        label='Role uživatele',
+    )
+
+    mac_address = forms.CharField(validators=[MinLengthValidator(17)], max_length=17, required=True, label='MAC adresa')
+    username = forms.CharField(max_length=20, required=True, label='Username')
     first_name = forms.CharField(max_length=20, required=True, label='Křestní jméno')
     last_name = forms.CharField(max_length=20, required=True, label='Příjmení')
-    mac_address = forms.CharField(validators=[MinLengthValidator(17)], max_length=17, required=True, label='Příjmení')
+    password = forms.CharField(widget=forms.PasswordInput)
+    
     
--- application/views.py
+++ application/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 from .forms import RegisterUserForm
+from .models import Student, Teacher
 
 def index(request):
     return render(request, 'index.html')
@@ -22,4 +24,28 @@ def login(request):
         return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            role = form.cleaned_data['role']
+            username = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            mac_address = form.cleaned_data['mac_address']
+            password = form.cleaned_data['password']
+            
+            user = User.objects.create_user(username=username, password=password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            
+            if role == 'Zak':
+                Student.objects.create(user=user, first_name=first_name, last_name=last_name, mac_address=mac_address)
+            elif role == 'Ucitel':
+                Teacher.objects.create(user=user, first_name=first_name, last_name=last_name, mac_address=mac_address)
+            
+            auth_login(request, user)
+            return redirect('/')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'signup.html', {'form': form})

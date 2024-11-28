1cd7388bc696e7267a7835f34f6fbb102f9048ea
sabi <sabina.javurkova@student.gyarab.cz>
2024-11-28 15:03:36 +0100

    zobrazovani postu

--- posty/models.py
+++ posty/models.py
@@ -4,3 +4,4 @@ class Report(models.Model):
     username = models.CharField(max_length=25, default='', blank=True)
     address = models.CharField(max_length=50)
     text = models.CharField(max_length=150)
+    date = models.DateField(auto_now_add=True)
--- posty/views.py
+++ posty/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Report
 
 def report(request):
-    return render(request, 'report-index.html')
+    return render(request, 'report-index.html', {})
 
 def zobrazit(request):
-    return render(request, 'zobrazit-index.html')
+    reports = Report.objects.all()
+    return render(request, 'zobrazit-index.html', {'reports': reports})

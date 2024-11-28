1ec13369a1aeb369a7bcf5c0247122d0b71a1a68
sabi <sabina.javurkova@student.gyarab.cz>
2024-11-28 20:43:20 +0100

    restart

--- posty/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Report
-
-class ReportAdmin(admin.ModelAdmin):
-    list_display = ('address', 'text', 'username')
-
-
-admin.site.register(Report, ReportAdmin)
--- posty/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('report', views.report, name='report-posty'),
-    path('zobrazit', views.zobrazit, name='zobrazit-posty'),
-]

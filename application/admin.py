f4ec68c4f2fe03ca12523e8450ac0c348c74f6a7
sabi <sabina.javurkova@student.gyarab.cz>
2024-11-28 22:26:42 +0100

    schema db ready

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student, Teacher, Lecture
+
+class LectureInline(admin.TabularInline):
+    model = Lecture
+    extra = 0
+    
+class LecturesInline(admin.TabularInline):
+    model = Lecture.students.through
+    extra = 0
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    inlines = [LecturesInline]
+    
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    inlines = [LectureInline]
+    
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'teacher')
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Lecture, LectureAdmin)
--- application/models.py
+++ application/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    mac_address = models.CharField(validators=[MinLengthValidator(17)], max_length=17)
+    is_present = models.BooleanField(default=False)
+    is_in_school = models.BooleanField(default=False)
+    
+    objects = models.Manager()
+    class LectureManager(models.Manager):
+        def create_lecture(self, name, students, teacher):
+            return self.create(name=name, students=students, teacher=teacher)
+    
+    lectures = LectureManager()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher_profile')
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    mac_address = models.CharField(validators=[MinLengthValidator(17)], max_length=17)
+    
+    objects = models.Manager()
+    class LectureManager(models.Manager):
+        def create_lecture(self, name, students, teacher):
+            return self.create(name=name, students=students, teacher=teacher)
+        
+    lectures = LectureManager()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+   
+    
+class Lecture(models.Model):
+    name = models.CharField(max_length=20)
+    students = models.ManyToManyField(Student, related_name='students')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='teacher')
+    
+    def __str__(self):
+        return f"{self.name} taught by {self.teacher.last_name}"
